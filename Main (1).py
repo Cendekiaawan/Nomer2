fe6f521e3af8fb9733bf6ccca85e65d2600370db
Nama Anda <email@anda.com>
2024-01-03 17:01:25 +0700

    Commit Pertama

---
+++ Main (1).py
@@ -0,0 +1,30 @@
+from Math_function import add, mul, div
+
+
+def main():
+
+    data_1 = int(input("masukkan input 1 :"))
+    data_2 = int(input("masukkan input 2 :"))
+    operator = input("masukkan operator :")
+
+    if operator == "+":
+        result = add(data_1, data_2)
+    elif operator == "*":
+        result = mul(data_1, data_2)
+    elif operator == "/":
+        result = div(data_1, data_2)
+
+    print("{} {} {} = {} ".format(data_1, operator, data_2, result))
+
+
+if __name__ == "__main__":
+    print("Hello Main !")
+    main()
+    ef add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
